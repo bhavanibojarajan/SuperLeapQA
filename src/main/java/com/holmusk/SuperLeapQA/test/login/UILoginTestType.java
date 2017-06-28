d933ff00ce0c4b8b4cb0a0f79d08cf920af18b7c
Hai Pham <viethai.pham@holmusk.com>
2017-06-28 17:29:35 +0800

    Fixed tests for loginInputsShouldWork to dismiss all alerts

--- src/main/java/com/holmusk/SuperLeapQA/test/login/UILoginTestType.java
+++ src/main/java/com/holmusk/SuperLeapQA/test/login/UILoginTestType.java
@@ -2,6 +2,7 @@ package com.holmusk.SuperLeapQA.test.login;
 
 import com.holmusk.SuperLeapQA.model.UserMode;
 import com.holmusk.SuperLeapQA.navigation.Screen;
+import com.holmusk.SuperLeapQA.navigation.type.ScreenInitializationType;
 import com.holmusk.SuperLeapQA.test.base.UIBaseTestType;
 import io.reactivex.Flowable;
 import io.reactivex.subscribers.TestSubscriber;
@@ -15,7 +16,11 @@ import java.util.List;
 /**
  * Created by haipham on 5/26/17.
  */
-public interface UILoginTestType extends UIBaseTestType, LoginActionType {
+public interface UILoginTestType extends
+    UIBaseTestType,
+    ScreenInitializationType,
+    LoginActionType
+{
     /**
      * Login with predefined credentials and verify that it works correctly.
      * @see ObjectUtil#nonNull(Object)
@@ -26,6 +31,8 @@ public interface UILoginTestType extends UIBaseTestType, LoginActionType {
      * @see #assertCorrectness(TestSubscriber)
      * @see #engine()
      * @see #rxa_login(Engine, List)
+     * @see #rxa_loginWithDefaults(Engine, UserMode)
+     * @see #rxn_dashboardTutorialInitialized(Engine)
      */
     @Test
     @SuppressWarnings("unchecked")
@@ -38,7 +45,10 @@ public interface UILoginTestType extends UIBaseTestType, LoginActionType {
         // When
         Flowable.concatArray(
             rxa_navigate(mode, Screen.SPLASH, Screen.LOGIN),
-            rxa_loginWithDefaults(engine, mode)
+            rxa_loginWithDefaults(engine, mode),
+
+            /* Call this method to make sure all alerts are dismissed */
+            rxn_dashboardTutorialInitialized(engine)
         ).all(ObjectUtil::nonNull).toFlowable().subscribe(subscriber);
 
         subscriber.awaitTerminalEvent();
@@ -49,10 +59,10 @@ public interface UILoginTestType extends UIBaseTestType, LoginActionType {
 
     /**
      * Verify that the register page can be accessed from the login page.
+     * @see UserMode#defaultUserMode()
      * @see Screen#SPLASH
      * @see Screen#LOGIN
      * @see Screen#REGISTER
-     * @see UserMode#defaultUserMode()
      * @see #assertCorrectness(TestSubscriber)
      * @see #rxa_navigate(UserMode, Screen...)
      * @see #rxv_registerScreen(Engine)
@@ -61,15 +71,15 @@ public interface UILoginTestType extends UIBaseTestType, LoginActionType {
     @SuppressWarnings("unchecked")
     default void test_loginToRegister_shouldWork() {
         // Setup
-        final UILoginTestType THIS = this;
-        final Engine<?> ENGINE = engine();
+        Engine<?> engine = engine();
         UserMode mode = UserMode.defaultUserMode();
         TestSubscriber subscriber = CustomTestSubscriber.create();
 
         // When
-        rxa_navigate(mode, Screen.SPLASH, Screen.LOGIN, Screen.REGISTER)
-            .flatMap(a -> THIS.rxv_registerScreen(ENGINE))
-            .subscribe(subscriber);
+        Flowable.concatArray(
+            rxa_navigate(mode, Screen.SPLASH, Screen.LOGIN, Screen.REGISTER),
+            rxv_registerScreen(engine)
+        ).all(ObjectUtil::nonNull).toFlowable().subscribe(subscriber);
 
         subscriber.awaitTerminalEvent();
 
